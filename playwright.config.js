fc04dafec33f8883e3cb38383f00a4d6fe23ffff
tunde <Tasiyan2001@yahoo.com>
2025-08-16 07:30:04 +0100

    third  commit

--- playwright.config.js
+++ playwright.config.js
@@ -2,10 +2,12 @@
 import { defineConfig, devices } from '@playwright/test';
 import { defineBddConfig } from 'playwright-bdd';
 
+
 const testDir = defineBddConfig({
   features: 'tests/features/login.feature',
   steps: ['tests/steps/loginStep.js', 'tests/fixtures/fixture.js'  ],
 });
+
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
@@ -28,7 +30,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter:[["line"], ["allure-playwright"],['html'], ['github']],
+  reporter:[["line"], ["allure-playwright"],['html'], ['github'],['@estruyf/github-actions-reporter'] ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
